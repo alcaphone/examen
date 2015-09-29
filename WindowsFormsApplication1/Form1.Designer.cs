3c6b61176ff4d2e408e63966bd96a77efb31b3c8
Jonathan <juancho_147-@hotmail.com>
2015-09-29 17:38:48 -0400

    Multiplicacion

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,12 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BTO_MULT = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TXT_DATO_1 = new System.Windows.Forms.TextBox();
+            this.TXT_DATO_2 = new System.Windows.Forms.TextBox();
+            this.RESPUESTA = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "INGRESE UN PRIMER NUMERO:";
+            // 
+            // BTO_MULT
+            // 
+            this.BTO_MULT.Location = new System.Drawing.Point(37, 287);
+            this.BTO_MULT.Name = "BTO_MULT";
+            this.BTO_MULT.Size = new System.Drawing.Size(101, 31);
+            this.BTO_MULT.TabIndex = 1;
+            this.BTO_MULT.Text = "MULTIPLICAR";
+            this.BTO_MULT.UseVisualStyleBackColor = true;
+            this.BTO_MULT.Click += new System.EventHandler(this.BTO_MULT_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "INGRESE UN SEGUNDO NUMERO:";
+            // 
+            // TXT_DATO_1
+            // 
+            this.TXT_DATO_1.Location = new System.Drawing.Point(199, 199);
+            this.TXT_DATO_1.Name = "TXT_DATO_1";
+            this.TXT_DATO_1.Size = new System.Drawing.Size(50, 20);
+            this.TXT_DATO_1.TabIndex = 3;
+            // 
+            // TXT_DATO_2
+            // 
+            this.TXT_DATO_2.Location = new System.Drawing.Point(199, 230);
+            this.TXT_DATO_2.Name = "TXT_DATO_2";
+            this.TXT_DATO_2.Size = new System.Drawing.Size(50, 20);
+            this.TXT_DATO_2.TabIndex = 4;
+            // 
+            // RESPUESTA
+            // 
+            this.RESPUESTA.AutoSize = true;
+            this.RESPUESTA.Location = new System.Drawing.Point(196, 287);
+            this.RESPUESTA.Name = "RESPUESTA";
+            this.RESPUESTA.Size = new System.Drawing.Size(13, 13);
+            this.RESPUESTA.TabIndex = 5;
+            this.RESPUESTA.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 342);
+            this.Controls.Add(this.RESPUESTA);
+            this.Controls.Add(this.TXT_DATO_2);
+            this.Controls.Add(this.TXT_DATO_1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BTO_MULT);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BTO_MULT;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TXT_DATO_1;
+        private System.Windows.Forms.TextBox TXT_DATO_2;
+        private System.Windows.Forms.Label RESPUESTA;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,14 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void BTO_MULT_Click(object sender, EventArgs e)
+        {
+            double num1, num2, resp;
+            num1 = double.Parse(TXT_DATO_1.Text);
+            num2 = double.Parse(TXT_DATO_2.Text);
+            resp = num1 * num2;
+            RESPUESTA.Text ="la solucion es:"+ resp;
+        }
     }
 }
